f268ce5fb7691ca82919598c83b2bf01c3b0f5ae
Gvozdev Leonid <leo.gvozdev.dev@mailbox.org>
2026-05-12 11:22:12 +0200

    updated

--- pythonforandroid/recipes/cryptography/__init__.py
+++ pythonforandroid/recipes/cryptography/__init__.py
@@ -11,12 +11,14 @@ class CryptographyRecipe(RustCompiledComponentsRecipe):
 
     def get_recipe_env(self, arch, **kwargs):
         env = super().get_recipe_env(arch, **kwargs)
+        python_link_version = self.ctx.python_recipe.link_version
         openssl_build_dir = self.get_recipe('openssl', self.ctx).get_build_dir(arch.arch)
         build_target = self.RUST_ARCH_CODES[arch.arch].upper().replace("-", "_")
         openssl_include = "{}_OPENSSL_INCLUDE_DIR".format(build_target)
         openssl_libs = "{}_OPENSSL_LIB_DIR".format(build_target)
         env[openssl_include] = join(openssl_build_dir, 'include')
         env[openssl_libs] = join(openssl_build_dir)
+        env["RUSTFLAGS"] += f" -Clink-arg=-lpython{python_link_version}"
         return env
 
 
